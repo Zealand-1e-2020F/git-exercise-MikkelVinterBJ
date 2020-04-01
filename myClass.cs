5663b3981b6d3d9f65de5d0de2edca12c97bdbaf
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-04-01 11:53:08 +0200

    tilføjet tostring method override

--- myClass.cs
+++ myClass.cs
@@ -7,11 +7,16 @@ namespace GitExercise
         private int postnr {get; set;}
     
 
-    public myClass(string navn, string adresse, int postnr)
-    {
+        public myClass(string navn, string adresse, int postnr)
+        {
         this.navn = navn;
         this.adresse = adresse;
         this.postnr = postnr;
+        }
+
+        public override string ToString()
+        {
+            return $"My name is {navn}, and i live in {adresse}, and my postnr is {postnr}";
+        }
     }
-}
 }
