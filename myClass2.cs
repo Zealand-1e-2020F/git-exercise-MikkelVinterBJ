7dc4b4b329f7ec249394b7375c3069fc83611d9c
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-04-01 13:06:02 +0200

    Revert "Update myClass2.cs"
    This reverts commit 1168127914377a483374691832dfa35ccf7210a5.

--- myClass2.cs
+++ myClass2.cs
@@ -2,7 +2,17 @@ namespace GitExercise
 {
     class myClass2
     {
-         
+        private string navn{ get;}
+        private string adresse {get;}
+        private int postnr {get;}
+        private string by {get;}
+    
+
+        public myClass2(string navn, string adresse, int postnr, string by)
+        {
+        this.navn = navn;
+        this.adresse = adresse;
+        this.postnr = postnr;
         this.by = by;
         }
 
