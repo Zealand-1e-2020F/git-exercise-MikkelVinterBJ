c79377b09cf7d5165c1610649100ba221731b50a
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-04-01 11:00:10 +0200

    Ny property og constructor
     Tilføjet property med navn og constructor med navn

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,12 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        public string navn{ get; set; }
+    
+
+    public myClass(string navn)
+    {
+        this.navn = navn;
     }
+}
 }
