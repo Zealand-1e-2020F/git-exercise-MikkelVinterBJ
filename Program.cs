9a63cd643cc597cd75d29fa56d3c7ea7c51d4242
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-04-01 12:44:40 +0200

    Tilføjer by argument til klasserne

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace GitExercise
         {
             Console.WriteLine("Hello World!");
 
-            myClass myclass = new myClass("Mikkel","Vonsildvej 2",2770);
-            myClass myclass2 = new myClass("Anna","Skovløbervej 30",2400);
-            myClass myclass3 = new myClass("Robin","Amager Strandvej 150",2770);
+            myClass myclass = new myClass("Mikkel","Vonsildvej 2",2770, "Kastrup");
+            myClass myclass2 = new myClass("Anna","Skovløbervej 30",2400, "København NV");
+            myClass myclass3 = new myClass("Robin","Amager Strandvej 150",2770, "Kastrup");
 
             Console.WriteLine(myclass);
             Console.WriteLine(myclass2);
