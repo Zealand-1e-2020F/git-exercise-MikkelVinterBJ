2c87bc82f8d376f378948cda5d2fbff4163c25e0
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-04-01 13:04:14 +0200

    Revert "Delete myClass.cs"
    This reverts commit 5882987f06f0ff37f989c9dc768a947b7a056eb7.

---
+++ myClass.cs
@@ -0,0 +1,24 @@
+namespace GitExercise
+{
+    class myClass
+    {
+        private string navn{ get;}
+        private string adresse {get;}
+        private int postnr {get;}
+        private string by {get;}
+    
+
+        public myClass(string navn, string adresse, int postnr, string by)
+        {
+        this.navn = navn;
+        this.adresse = adresse;
+        this.postnr = postnr;
+        this.by = by;
+        }
+
+        public override string ToString()
+        {
+            return $"Mit navn er {navn} og jeg bor på {adresse}, min by hedder {by} og mit postnummer er {postnr}";
+        }
+    }
+}
