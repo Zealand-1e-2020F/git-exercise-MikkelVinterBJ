6d552373429e0b5d1b40f9ecbc6b2e3e4c455652
MikkelVinterBJ <vbjmikkel@gmail.com>
2020-04-01 11:10:00 +0200

    tilføjet adresse og postnr property

--- myClass.cs
+++ myClass.cs
@@ -3,6 +3,8 @@ namespace GitExercise
     class myClass
     {
         public string navn{ get; set; }
+        public string adresse {get; set;}
+        public int postnr {get; set;}
     
 
     public myClass(string navn)
